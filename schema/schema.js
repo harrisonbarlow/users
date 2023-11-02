8f5f269bc3f68af31c33d85f34c701413d020316
Harrison Barlow <harrison.barlow@bhp.com>
2023-11-02 20:23:19 +0800

    More schema work

--- schema/schema.js
+++ schema/schema.js
@@ -6,16 +6,41 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+    name: 'Company',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString },
+        description: { type: GraphQLString },
+        users: { 
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(response => response.data);
+            }
+        }
+    })
+});
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
-        age: { type:GraphQLInt }
-    }
+        age: { type: GraphQLInt },
+        company: { 
+            type: CompanyType,
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+                    .then(response => response.data);
+            }
+        }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -28,10 +53,61 @@ const RootQuery = new GraphQLObjectType({
                 return axios.get(`http://localhost:3000/users/${args.id}`)
                     .then(response => response.data);
             }
+        },
+        company: {
+            type: CompanyType,
+            args: { id: { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/companies/${args.id}`)
+                    .then(response => response.data);
+            }
         }
+    }
+});
+
+const mutation = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                // GraphQLNonNull makes sure that the argument is not null
+                firstName: { type: GraphQLNonNull(GraphQLString) },
+                age: { type: GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { firstName, age }) {
+                return axios.post(`http://localhost:3000/users`, { firstName, age })
+                    .then(response => response.data);
+            }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parentValue, { id }) {
+                return axios.delete(`http://localhost:3000/users/${id}`)
+                    .then(response => response.data);
+            }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLNonNull(GraphQLString) },
+                age: { type: GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { id, firstName, age, companyId }) {
+                return axios.patch(`http://localhost:3000/users/${id}`, { firstName, age, companyId })
+                    .then(response => response.data);
+            }
+        }
+    
     }
 });
 
 module.exports = new GraphQLSchema({
     query: RootQuery
-})
+});
